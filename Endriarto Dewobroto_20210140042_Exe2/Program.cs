7aa2d31193afcab524dea27027fe9e6a5100b198
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-11-02 15:21:39 +0700

    Add project files.

---
+++ Endriarto Dewobroto_20210140042_Exe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endriarto_Dewobroto_20210140042_Exe2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
